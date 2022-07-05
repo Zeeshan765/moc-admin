6be6f00f921d1530ef024832d05bcdf990ef318a
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2022-07-05 14:48:55 +0500

    complete

--- src/components/AdminWrapper/Admin.jsx
+++ src/components/AdminWrapper/Admin.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import apiService from '../../services/ApiService'
 import { withRouter } from "react-router";
+import { toast } from "react-toastify";
+
+// import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 const Admin = (props) => {
     React.useEffect(() => {
       if (!apiService.isAdmin() ) {
+        // toast.error("You are not authorized to access this page", {
+        //   position: toast.POSITION.TOP_LEFT,
+        //   theme: "colored",
+        //   });
         props.history.push("/Login");
       }
       
