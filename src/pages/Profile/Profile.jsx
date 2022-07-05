3b628fe116f32b9ddbf69e53fb99d1b0138005a0
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2022-07-05 07:46:06 +0500

    profile add edit

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import apiService from "../../services/ApiService";
-import { useState } from "react";
-import { toast } from "react-toastify";
-import "./Profile.css";
-import { makeStyles } from "@material-ui/core/styles";
-import { TextField, Button } from "@material-ui/core";
-import Link from "@material-ui/core/Link";
-import Avatar from "@material-ui/core/Avatar";
+import React from 'react';
+import apiService from '../../services/ApiService';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import './Profile.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Button } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import Avatar from '@material-ui/core/Avatar';
 //import Paper from '@material-ui/core/Paper';
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-
-import { deepOrange } from "@material-ui/core/colors";
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import validator from 'validator';
+import { deepOrange } from '@material-ui/core/colors';
 //import swal from "sweetalert";
 export default function Profile(props) {
   //Get Logged In User Data
   React.useEffect(() => {
-    apiService.get("api/user/find").then((res) => {
+    apiService.get('api/user/find').then((res) => {
       console.log(res.data);
       setName(res.data.name);
       setEmail(res.data.email);
@@ -24,59 +24,110 @@ export default function Profile(props) {
     });
   }, []);
 
+  function EditsEnabled() {
+    setEdit(true);
+  }
+  const validateEmail = (email) => {
+    if (validator.isEmail(email)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  function EditsDisabled() {
+    setEdit(false);
+  }
+  const emailValidation = (email) => {
+    if (validator.isEmail(email)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const userNameValidation = (name) => {
+    if (name.length >= 5 && name.length <= 15) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const phoneValidation = (phone) => {
+    if (
+      phone.length === 11 &&
+      phone.startsWith('03') &&
+      phone.match(/^[0-9]+$/)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  function allValidation(name, email, phone) {
+    console.log(name, email, phone);
+    if (
+      userNameValidation(name) &&
+      emailValidation(email) &&
+      phoneValidation(phone)
+    ) {
+      handleupdate(name, email, phone);
+    } else {
+      toast.error('Please Enter Valid Data');
+    }
+  }
+
   function handleupdate() {
     apiService
-      .put("/api/user/profile", { name, email, phone })
+      .put('/api/user/profile', { name, email, phone })
       .then((res) => {
         console.log(res);
-        toast.success("Profile Updated Successfully", {
+        toast.success('Profile Updated Successfully', {
           position: toast.POSITION.TOP_LEFT,
-          theme: "colored",
+          theme: 'colored',
         });
-        props.history.push("/components");
+        props.history.push('/components');
       })
       .catch((error) => {
         console.log(error.message);
         toast.error(error.response.data, {
           position: toast.POSITION.TOP_LEFT,
-          theme: "colored",
+          theme: 'colored',
         });
       });
   }
   function handlechange() {
-    props.history.push("/update/password");
+    props.history.push('/update/password');
   }
   const useStyles = makeStyles((theme) => ({
     root: {
-      marginTop: "-30px",
-      alignSelf: "center",
-      alignItems: "center",
-      justifyContent: "center",
-      flexWrap: "wrap",
-      width: "50%",
+      marginTop: '-30px',
+      alignSelf: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      width: '50%',
 
-      margin: "auto",
+      margin: 'auto',
 
-      padding: "10px",
-      backgroundColor: "#fff",
-      borderRadius: "10px",
-      boxShadow: "0px 0px 10px #000",
+      padding: '10px',
+      backgroundColor: '#fff',
+      borderRadius: '10px',
+      boxShadow: '0px 0px 10px #000',
     },
     image: {
       backgroundImage: `url(${Image})`,
-      backgroundRepeat: "no-repeat",
+      backgroundRepeat: 'no-repeat',
       backgroundColor:
-        theme.palette.type === "dark"
+        theme.palette.type === 'dark'
           ? theme.palette.grey[50]
           : theme.palette.grey[900],
-      backgroundSize: "cover",
-      backgroundPosition: "center",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
     },
     paper: {
       margin: theme.spacing(8, 6),
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
     },
     avatar: {
       margin: theme.spacing(1),
@@ -84,54 +135,76 @@ export default function Profile(props) {
       backgroundColor: deepOrange[500],
       width: 86,
       height: 86,
-      marginTop: "30px",
+      marginTop: '30px',
     },
     form: {
-      width: "100%",
+      width: '100%',
       marginTop: theme.spacing(1),
     },
     submit: {
       margin: theme.spacing(3, 0, 2),
     },
-    textField: {
-      width: "100%",
-      marginLeft: "auto",
-      marginRight: "auto",
-      paddingBottom: 0,
-      marginTop: "50px",
-      fontWeight: 500,
-    },
+    textField:
+    {  "&:disabled": {
+     color: "white",
+     backgroundColor: "#fff",
+     
+     
+   }
+   ,
+     
+     width: "100%",
+     marginLeft: "auto",
+     marginRight: "auto",
+     paddingBottom: 0,
+     marginTop: "50px",
+     fontWeight: 500,
+   },
     input: {
-      color: "white",
-      backgroundColor: "#362245",
+      '&:disabled': {
+        color: 'black',
+        backgroundColor: '#fff',
+      },
+      color: 'white',
+      backgroundColor: '#362245',
       height: 80,
-      fontSize: "25px",
+      fontSize: '25px',
     },
+
     button: {
+      backgroundColor: '#362245',
+      fontSize: '20px',
+      fontWeight: 500,
+      color: 'white',
+      padding: '30px',
+      marginLeft: '10px',
+    },
+    Cpbutton: {
       backgroundColor: "#362245",
       fontSize: "20px",
       fontWeight: 500,
       color: "white",
       padding: "30px",
       marginLeft: "10px",
+      marginTop: "10px",
     },
     checkBox: {
-      color: "white",
+      color: 'white',
     },
     Link: {
-      color: "white",
-      fontSize: "18px",
-      marginTop: "40px",
+      color: 'white',
+      fontSize: '18px',
+      marginTop: '40px',
     },
     SignText: {
-      color: "white",
-      fontSize: "42px",
-      marginTop: "5px",
+      color: 'white',
+      fontSize: '42px',
+      marginTop: '5px',
     },
 
     floatingLabelFocusStyle: {
-      color: "white",
-      fontSize: "20px",
+      color: 'white',
+      fontSize: '20px',
     },
 
     orange: {
@@ -139,13 +212,15 @@ export default function Profile(props) {
       backgroundColor: deepOrange[500],
     },
   }));
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [edit, setEdit] = useState(false);
+
   const classes = useStyles();
 
   return (
-    <div className="container">
+    <div className='container'>
       {/* <div>
         <input
           className="pass"
@@ -176,10 +251,10 @@ export default function Profile(props) {
           }}
         />
       </div> */}
-      <Grid container component="main" className={classes.root}>
+      <Grid container component='main' className={classes.root}>
         {/* <Grid item md={7} className={classes.image} /> */}
-        <Grid item style={{ backgroundColor: "#180c2b" }} md={12}>
-          <div style={{ color: "#474745" }} className={classes.paper}>
+        <Grid item style={{ backgroundColor: '#180c2b' }} md={12}>
+          <div style={{ color: '#474745' }} className={classes.paper}>
             <Avatar
               sm={{ width: 86, height: 86 }}
               className={classes.avatar}
@@ -187,95 +262,130 @@ export default function Profile(props) {
             ></Avatar>
             <Typography
               className={classes.SignText}
-              component="h1"
-              variant="h5"
+              component='h1'
+              variant='h5'
             >
               My Profile
             </Typography>
             <form className={classes.form} noValidate>
               <TextField
+                style={{ backgroundColor: '#362245' }}
                 className={classes.textField}
                 InputLabelProps={{ className: classes.floatingLabelFocusStyle }}
                 InputProps={{
                   className: classes.input,
                 }}
-                variant="filled"
-                margin="normal"
+                variant='filled'
+                margin='normal'
                 value={email}
-                color="secondary"
+                color='secondary'
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
                 autoFocus
+                disabled={!edit}
+
                 onChange={(e) => {
                   setEmail(e.target.value);
                 }}
+                helperText={
+                  emailValidation(email)
+                    ? " "
+                    : "Email is not valid"
+                }
+                error={!emailValidation(email)}
               />
               <br />
               <TextField
+                             style={{ backgroundColor: "#362245" }}
+
                 className={classes.textField}
                 InputLabelProps={{ className: classes.floatingLabelFocusStyle }}
                 InputProps={{
                   className: classes.input,
                 }}
-                variant="filled"
-                margin="normal"
-                color="secondary"
+                variant='filled'
+                margin='normal'
+                color='secondary'
                 fullWidth
+                disabled={!edit}
                 value={name}
-                id="name"
-                label="User Name"
-                name="name"
-                autoComplete="name"
+                id='name'
+                label='User Name'
+                name='name'
+                autoComplete='name'
                 autoFocus
                 onChange={(e) => {
                   setName(e.target.value);
                 }}
+                helperText={
+                  userNameValidation(name)
+                    ? " "
+                    : "User Name must be between 5 to 15 characters"
+                }
+                error={!userNameValidation(name)}
               />
               <br />
               <TextField
+                                  style={{ backgroundColor: "#362245" }}
+
                 className={classes.textField}
                 InputLabelProps={{ className: classes.floatingLabelFocusStyle }}
                 InputProps={{
                   className: classes.input,
                 }}
-                variant="filled"
-                margin="normal"
-                color="secondary"
+                variant='filled'
+                margin='normal'
+                color='secondary'
                 fullWidth
+                disabled={!edit}
                 value={phone}
-                id="Phone"
-                label="Phone"
-                name="Phone"
-                autoComplete="Phone"
+                id='Phone'
+                label='Phone'
+                name='Phone'
+                autoComplete='Phone'
                 autoFocus
                 onChange={(e) => {
                   setPhone(e.target.value);
                 }}
+                helperText={
+                  phoneValidation(phone)
+                    ? " "
+                    : "Must begin with 03 and 11 digits"
+                }
+                error={!phoneValidation(phone)}
               />
               <Grid
                 container
-                justify="flex-end"
+                justify='flex-end'
                 spacing={4}
                 style={{ padding: 20 }}
               ></Grid>
 
               <Grid
                 container
-                justify="space-around"
+                justify='space-around'
                 spacing={4}
                 style={{ padding: 20 }}
               ></Grid>
-              <Button className={classes.button} onClick={handleupdate}>
+              <div>
+              <Button className={classes.button} onClick={EditsEnabled}>
+                {" "}
+                Edit{" "}
+              </Button>
+              <Button
+              disabled={!edit}
+               className={classes.button} onClick={()=>allValidation(name,email,phone)}>
                 {" "}
                 Update{" "}
               </Button>
               <Button className={classes.button} onClick={handlechange}>
-                {" "}
-                Change Password{" "}
+                {' '}
+                Change Password{' '}
               </Button>
+              </div>
             </form>
           </div>
         </Grid>
